324c5d10a6f0dcbff72ab53164bdd093cea70667
Mahitha Appasani <mahiappasani@yahoo.com>
2021-06-14 14:35:18 -0400

    restoring

--- get-data.py
+++ get-data.py
@@ -4,7 +4,6 @@ import yfinance as yf
 yf.pdr_override()
 import pandas as pd
 
-
 ticker_list = ['DJIA','DOW','LB','EXPE','PXD','MHCP','CRM','JEC','NRG','HFC','NOW']
 
 today=date.today()
@@ -12,27 +11,25 @@ today=date.today()
 start_date='2019-11-30'
 end_date = '2021-5-31'
 
+files=[]
 def getData(ticker):
-    data = pdr.get_data_yahoo(ticker,start=start_date,end=end_date)
-    data.insert(1,'Stock',ticker)
-    data['Change'] = data['Open'] - data['Close'] 
-    data.reset_index(inplace=True)
-    data = data.rename(columns = {'Stock': 'stock','Open': 'value', 'Change': 'change'})
-    #with open('csv_data.csv','w') as csv_file:
-        #data.to_csv(columns=['Date','stock','value','change'],path_or_buf=csv_file)
+    print(ticker)
+    data=pdr.get_data_yahoo(ticker,start=start_date,end=today)
+    dataname=ticker+'_'+str(today)
+    files.append(dataname)
+    SaveData(data,dataname)
 
 def SaveData(df,filename):
     df.to_csv('./data/'+filename+'.csv')
 
-stock = 'MSFT'
-getData(stock)
-
 #for tik in ticker_list:
-   #getData(stock)
+ #   getData(tik)
 #for i in range(0,10):
  #   df1 = pd.read_csv('./data/'+str(files[i])+'.csv')
   #  print(df1.head())
 
+stock = 'MSFT'
+data = pdr.get_data_yahoo(stock,start=start_date,end=end_date)
 #get date from header and insert into data
 data.insert(1,'Stock',stock)
 #print(len(data))
@@ -42,3 +39,10 @@ data.insert(1,'Stock',stock)
  #   stocklist.append(stock)
   #  i = i+1
 #data['Stock'] = stocklist
+data['Change'] = data['Open'] - data['Close'] 
+data.reset_index(inplace=True)
+data = data.rename(columns = {'Stock': 'stock','Open': 'value', 'Change': 'change'})
+output = data.to_csv(columns=['Stock','Open','Change'])
+with open('csv_data.csv','w') as csv_file:
+    data.to_csv(columns=['Date','stock','value','change'],path_or_buf=csv_file)
+#print(output)
